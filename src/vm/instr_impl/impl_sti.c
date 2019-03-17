46edc7f9420476bd02dbc3d50d6a13d0ec2cdc74
Alex Amadori <aamadori@42.student.fr>
2019-03-17 19:07:12 +0100

    Implemented sti, fixed address restriction for ldi

--- src/vm/instr_impl/impl_sti.c
+++ src/vm/instr_impl/impl_sti.c
@@ -1,43 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   impl_ldi.c                                         :+:      :+:    :+:   */
+/*   impl_sti.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/15 19:55:16 by aamadori          #+#    #+#             */
-/*   Updated: 2019/03/17 19:05:28 by alex             ###   ########.fr       */
+/*   Created: 2019/03/17 18:37:25 by alex              #+#    #+#             */
+/*   Updated: 2019/03/17 19:06:49 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 #include "cpu.h"
 
-void	impl_ldi(t_vm_state *state, t_process *process, t_instr *instr)
+void	impl_sti(t_vm_state *state, t_process *process, t_instr *instr)
 {
 	t_bigend_buffer	first_param;
 	t_bigend_buffer	second_param;
 	size_t			address;
 
-	if (instr->instr_args[0].arg_type == e_register)
-		first_param = process->registers[instr->instr_args[0].arg.reg_index].content;
-	else if (instr->instr_args[0].arg_type == e_index)
+	if (instr->instr_args[1].arg_type == e_register)
+		first_param = process->registers[instr->instr_args[1].arg.reg_index].content;
+	else if (instr->instr_args[1].arg_type == e_index)
 	{
 		address = *(size_t*)byte_order_swap(
-			instr->instr_args[0].arg.index.content, IND_SIZE).buffer;
+			instr->instr_args[1].arg.index.content, IND_SIZE).buffer;
 		address = process->program_counter + (address % IDX_MOD);
 		/* TODO what size to load? */
 		first_param = mem_load(state, address, IND_SIZE);
 	}
 	else
-		first_param = instr->instr_args[0].arg.direct.content;
-	if (instr->instr_args[1].arg_type == e_register)
-		second_param = process->registers[instr->instr_args[1].arg.reg_index].content;
+		first_param = instr->instr_args[1].arg.direct.content;
+	if (instr->instr_args[2].arg_type == e_register)
+		second_param = process->registers[instr->instr_args[2].arg.reg_index].content;
 	else
-		second_param = instr->instr_args[1].arg.direct.content;
+		second_param = instr->instr_args[2].arg.direct.content;
 	address = *(size_t*)byte_order_swap(
 		 add_bigend(first_param, second_param, REG_SIZE), REG_SIZE).buffer;
 	address = process->program_counter + (address % IDX_MOD);
-	process->registers[instr->instr_args[2].arg.reg_index].content
-		= mem_load(state, address, REG_SIZE);
+	mem_store(state, address, REG_SIZE,
+		process->registers[instr->instr_args[0].arg.reg_index].content);
 }
