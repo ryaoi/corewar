8af2247f78ba7d139edfe7a57e19a3c6dfa344be
Misaki NISHIMOTO <mnishimo@e2r1p4.42.fr>
2019-04-09 21:39:06 +0200

    timeout function added

--- src/flask/corewar.py
+++ src/flask/corewar.py
@@ -28,20 +28,23 @@ def prepare():
     game = libcore.CorewarGame()
     game_id = create_game_id()
     sd = SessionData(game, game_id)
-    file_list = request.files.items() 
+    file_list = request.files
     if not file_list:
        return "cannot get a list of files", 400
     i = 1
-    for f in file_list:
+    while i < 5:
+        f = file_list.get("file" + str(i))
+        if not f:
+            break;
         if i == 0:
-            sd.p1 = f[0]
+            sd.p1 = f.filename
         elif i == 1:
-            sd.p2 = f[0]
+            sd.p2 = f.filename
         elif i == 2:
-            sd.p3 = f[0]
+            sd.p3 = f.filename
         elif i == 3:
-            sd.p4 = f[0]
-        request.files.get(f[0]).save("players/" + game_id[:100] +"p" + str(i)+ ".cor")
+            sd.p4 = f.filename
+        f.save("players/" + game_id[:100] +"p" + str(i)+ ".cor")
         game.champions.append("players/" + game_id[:100] +"p" + str(i)+ ".cor")
         i += 1
     sessions.append(sd)
@@ -62,36 +65,43 @@ def create_game_id():
 
 @app.route("/AJAX/update/", methods=['POST'])
 def update():
-    game = None
-    info = json.loads(play_info)
-    game_id = info["game_id"]
-    now = time.time()
-    for x in sessions:
-        if x.game_id == game_id:
-            game = x.game
-            x.atime = now
+    signal.signal(signal.SIGALRM, raise_timeout)
+    signal.alarm(60000)
+    try:
+        game = None
+        info = json.loads(play_info)
+        game_id = info["game_id"]
+        now = time.time()
+        for x in sessions:
+            if x.game_id == game_id:
+                game = x.game
+                x.atime = now
        # if now - x.atime > timedelta(minutes=10):
        #     sessions.pop(x)
-    if game == None:
-        return ""
-    cycle = int(info["cycle"])
-    for i in range(cycle):
-        game.update()
-    mem_dump = game.mem_dump().hex()
-    logs_nbr = info["active_logs"]
-    active_logs = []
-    i = 0
-    while i < len(logs_nbr):
-        active_logs = active_logs + game.logs[int(logs_nbr[i])]
-        i += 1
-    active_logs.sort(key=lambda x: x[1])
-    for x in game.logs:
-        x.clear()
-    context = {
-            "mem": mem_dump,
-            "log": active_logs
-            }
-    return json.dumps(context)
+        if game == None:
+            return "no game found with the game_id", 400
+        cycle = int(info["cycle"])
+        if cycle > 500:
+            return "maximum cycle allowd : 500", 400
+        for i in range(cycle):
+            game.update()
+        mem_dump = game.mem_dump().hex()
+        logs_nbr = info["active_logs"]
+        active_logs = []
+        i = 0
+        while i < len(logs_nbr):
+            active_logs = active_logs + game.logs[int(logs_nbr[i])]
+            i += 1
+        active_logs.sort(key=lambda x: x[1])
+        for x in game.logs:
+            x.clear()
+        context = {
+                "mem": mem_dump,
+                "log": active_logs
+                }
+        return json.dumps(context)
+    except:
+        return "timeout.", 400
 
 @app.route("/AJAX/logout/", methods=['POST'])
 def end_game():
