8b87c1441625e7a78a748c4f0c6c16994534c83b
Jae Seok Lee <jslee062749@gmail.com>
2019-04-19 19:35:25 +0200

    coloring implemented, highlight the most recent execution needed

--- src/vm/visualizer/memory_dump.c
+++ src/vm/visualizer/memory_dump.c
@@ -6,24 +6,47 @@
 /*   By: jaelee <jaelee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 12:03:26 by jaelee            #+#    #+#             */
-/*   Updated: 2019/04/18 22:23:05 by jaelee           ###   ########.fr       */
+/*   Updated: 2019/04/19 19:34:31 by jaelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "visualizer.h"
+
+static int	get_champion_color(size_t index, t_byte_info info, size_t cycle_count)
+{
+	int	color;
+
+	(void)cycle_count;
+	(void)index;
+	if (info.written_player == -1)
+		color = 3;
+	else if (info.written_player == -2)
+		color = 4;
+	else if (info.written_player == -3)
+		color = 5;
+	else if (info.written_player == -4)
+		color = 6;
+
+	else
+		color = 2;
+	/* TODO highlight */
+	return (color);
+}
 
 static void output_memory_dump(t_vm_state *vm)
 {
 	size_t	index;
 	size_t	col;
 	size_t	row;
+	int		color;
 
 	index = 0;
 	col = 0;
 	row = 0;
 	while (index < MEM_SIZE)
 	{
-		wattron(win.mem_dump, COLOR_PAIR(2));
+		color = get_champion_color(index, vm->memory_info[index], vm->cycle_count);
+		wattron(win.mem_dump, COLOR_PAIR(color));
 		mvwprintw(win.mem_dump, (2 + row), 3 + (col * 3), "%02X", vm->memory[index]);
 		col++;
 		index++;
@@ -33,6 +56,7 @@ static void output_memory_dump(t_vm_state *vm)
 			row++;
 		}
 	}
+	wattroff(win.info, COLOR_PAIR(1));
 }
 
 void		create_memory_dump(t_vm_state *vm)
--- src/vm/visualizer/visualizer_backup.c
+++ src/vm/visualizer/visualizer_backup.c
@@ -6,7 +6,7 @@
 /*   By: jaelee <jaelee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/19 19:55:19 by aamadori          #+#    #+#             */
-/*   Updated: 2019/04/19 16:11:16 by jaelee           ###   ########.fr       */
+/*   Updated: 2019/04/19 19:25:36 by jaelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@
 void	close_ncurse(t_window *win)
 {
 	nodelay(stdscr, FALSE);
+	wattron((*win).info, COLOR_PAIR(2));
+	mvwprintw((*win).info, 10, 3, "GET THE FUCK OUT WITH ANY KEY!!");
+	wrefresh((*win).info);
 	getch();
-	(void)win;
-	printf("\ndelwin(mem_dump) returns : %d\n", delwin((*win).mem_dump));
-	printf("\ndelwin() returns : %d\n", delwin((*win).info));
-	printf("\nendwin returns : %d\n", endwin());
+	delwin((*win).mem_dump);
+	delwin((*win).info);
+	endwin();
 }
 
 int		main(void)
@@ -31,14 +33,20 @@ int		main(void)
 	t_array		players;
 	t_player	sha1;
 	t_player	sha2;
+	t_player	sha3;
+	t_player	sha4;
 	t_log_info	info;
 
 	if (vm_champion_load_file(&sha1, "/Users/jaelee/42/corewar/champs/Gagnant.cor", -1) < 0
-		|| vm_champion_load_file(&sha2, "/Users/jaelee/42/corewar/champs/Octobre_Rouge_V4.2.cor", -2) < 0)
+		|| vm_champion_load_file(&sha2, "/Users/jaelee/42/corewar/champs/Octobre_Rouge_V4.2.cor", -2) < 0
+		|| vm_champion_load_file(&sha3, "/Users/jaelee/42/corewar/death.cor", -3) < 0
+		|| vm_champion_load_file(&sha4, "/Users/jaelee/42/corewar/champs/fluttershy.cor", -4) < 0)
 		return (0);
 	array_init(&players, sizeof(t_player));
 	array_push_back(&players, &sha1);
 	array_push_back(&players, &sha2);
+	array_push_back(&players, &sha3);
+	array_push_back(&players, &sha4);
 	logs_init(&info);
 	info.log_mode = e_mode_stderr;
 	ft_memcpy(&info.log_active,
@@ -53,8 +61,6 @@ int		main(void)
 		if (visualizer(game) == -1)
 			break ;
 	}
-	printf("\naddr of win.memdump at end : %p\n", win.mem_dump);
-	printf("\naddr of win.info at end : %p\n", win.info);
 	close_ncurse(&win);
 //	ft_printf("%d %d\n", ARRAY_PTR(game->state.players, t_player)[0].live, ARRAY_PTR(game->state.players, t_player)[1].live);
 	return (0);
