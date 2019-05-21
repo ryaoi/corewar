aa4ebde23251f9c90b70bb8f7d9924a4b0cd0c28
Jae Seok Lee <jslee062749@gmail.com>
2019-05-21 19:06:15 +0200

    minor norm fix

--- src/vm/visualizer/visualizer_utils.c
+++ src/vm/visualizer/visualizer_utils.c
@@ -6,7 +6,7 @@
 /*   By: jaelee <jaelee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/07 16:07:19 by jaelee            #+#    #+#             */
-/*   Updated: 2019/05/21 19:00:47 by jaelee           ###   ########.fr       */
+/*   Updated: 2019/05/21 19:05:09 by jaelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,12 +36,14 @@ void	init_visualizer(t_visualizer_state *vis_state)
 	nodelay(stdscr, TRUE);
 	keypad(stdscr, TRUE);
 	vis_state->win.mem_dump = newwin(4 + (MEM_SIZE / 64), MEM_DUMP_WIDTH, 0, 0);
-	vis_state->win.info = newwin(4 + (MEM_SIZE / 64), INFO_WIDTH, 0, MEM_DUMP_WIDTH - 1);
+	vis_state->win.info =
+		newwin(4 + (MEM_SIZE / 64), INFO_WIDTH, 0, MEM_DUMP_WIDTH - 1);
 	ft_bzero(&vis_state->input_info, sizeof(vis_state->input_info));
 	vis_state->input_info.speed = 500;
 	vis_state->input_info.pause = 1;
 	vis_state->shutdown = 0;
 	vis_state->game_over = 0;
-	pthread_create(&vis_state->input_worker, NULL, input_loop_wrapper, vis_state);
+	pthread_create(&vis_state->input_worker, NULL,
+		input_loop_wrapper, vis_state);
 	pthread_mutex_init(&vis_state->input_lock, NULL);
 }
